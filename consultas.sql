357b4446d0176bf63f1a7d17e6a70fb696736afb
Camilo Hernandez <145624469+Camih1005@users.noreply.github.com>
2024-06-07 21:58:22 -0500

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -113,3 +113,106 @@ order by cantidad desc limit 4;
 específico
 */
 
+select pz.id as pieza,count(pz.nombre) as cantidad from pieza as pz
+left join reparacion_servicio as rs on rs.idPieza = pz.id
+join reparacion as r on r.id = rs.idReparacion
+where year(fechaIngreso) between "2015" and "2024"
+group by pieza
+order by pieza;
+
+/*
+14. Calcular el promedio de costo de reparaciones por vehículo
+*/
+
+select idVehiculo,avg(total) as promedio from reparacion
+group by idVehiculo;
+
+/*
+15. Obtener el inventario de piezas por proveedor
+*/
+
+select pp.idPieza,pp.idProveedor,sum(it.cantidad) as cantidad from proveedor_pieza as pp
+right join inventario_taller as it on it.idPieza = pp.idPieza
+group by pp.idPieza,pp.idProveedor ;
+
+
+/*
+16. Listar los clientes que no han realizado reparaciones en el último año
+*/
+
+select cl.id,r.fechaIngreso from reparacion as r
+join vehiculo as vh on r.idVehiculo = vh.id
+join cliente as cl on cl.id = vh.idCliente
+where year(r.fechaIngreso) < "2024" ;
+
+/*
+17. Obtener las ganancias totales del taller en un período específico
+*/
+
+SELECT 
+SUM(total) as ganancia,(CASE 
+WHEN YEAR(fechaIngreso) = year(fechaIngreso) THEN year(fechaIngreso)
+ELSE 'otra fecha'
+END) AS "año"FROM reparacion
+WHERE YEAR(fechaIngreso) BETWEEN 2022 AND 2024
+GROUP BY fechaIngreso;
+
+
+/*
+18. Listar los empleados y el total de horas trabajadas en reparaciones en un
+período específico (asumiendo que se registra la duración de cada reparación)
+*/
+
+select em.id,sum(re.horasTrabajadas) as horas from reparacion_empleado as re
+join empleado as em on em.id = re.idEmpleado
+join reparacion as r on r.id = re.idReparacion
+where year(r.fechaEntrega) between "2010" and "2022"
+group by em.id;
+
+/*
+19. Obtener el listado de servicios prestados por cada empleado en un período
+específico
+*/
+select re.idEmpleado,s.nombre from reparacion_servicio as rs
+join reparacion as r on r.id = rs.idReparacion
+join reparacion_empleado as re on re.idReparacion = r.id
+join servicio as s on s.id = rs.idServicio
+where year(r.fechaIngreso) between "2013" and "2024"
+order by re.idEmpleado;
+
+
+/*
+SUBCONSULTASS
+*/
+
+/*1. Obtener el cliente que ha gastado más en reparaciones durante el último año.*/
+
+select idCliente,max(total) as total from 
+(select idCliente,total from facturacion) as cli
+group by idCliente
+order by total desc limit 1;
+
+/*
+2. Obtener la pieza más utilizada en reparaciones durante el último mes
+*/
+SELECT rs.idPieza, COUNT(rs.idPieza) AS cantidad
+FROM reparacion_servicio AS rs
+JOIN reparacion AS r ON r.id = rs.idReparacion
+WHERE date(r.fechaIngreso) = (select max(date(r.fechaIngreso)))
+GROUP BY rs.idPieza
+ORDER BY COUNT(rs.idPieza) DESC
+limit 1;
+
+/*
+3. Obtener los proveedores que suministran las piezas más caras
+*/
+
+select pp.idPieza,pp.idProveedor,sum(it.cantidad),pz.precio as cantidad from proveedor_pieza as pp
+right join inventario_taller as it on it.idPieza = pp.idPieza
+join (select id,precio from pieza ) as pz on pz.id = it.idPieza
+group by pp.idPieza,pp.idProveedor,pz.precio
+order by pz.precio desc limit 3 ;
+
+
+select * from pieza ;
+
